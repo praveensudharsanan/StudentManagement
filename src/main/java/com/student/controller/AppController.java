df88b67a7d6fe0c16dcc694260b452b1c963abd9
Praveen S <praveensudharsanan@gmail.com>
2022-04-17 22:26:21 -0400

    Spring project login done

---
+++ src/main/java/com/student/controller/AppController.java
@@ -0,0 +1,45 @@
+package com.student.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.student.model.User;
+import com.student.repository.UserRepository;
+ 
+@Controller
+public class AppController {
+ 
+    @Autowired
+    private UserRepository userRepo;
+     
+    @GetMapping("")
+    public String viewHomePage() {
+        return "index";
+    }
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
+         
+        return "signup_form";
+    }
+    
+    @PostMapping("/process_register")
+    public String processRegister(User user) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+        userRepo.save(user);
+         
+        return "process_register";
+    }
+    
+    @GetMapping("/login")
+    public String showLoginForm(Model model) {
+        model.addAttribute("user", new User());
+        return "login";
+    }
+}
